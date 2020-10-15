1e0b0f59d2cb832f76cdc134a586c482425619e5
vaguila95 <vlaguila@uc.cl>
2020-10-15 03:28:33 -0300

    :white_check_mark: os_open test main

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -4,7 +4,8 @@ int main(int argc, char *argv[])
 {
   os_mount("data/simdiskfilled.bin");
   // os_bitmap(0, true);
-  osFile *file = os_open("music", "r");
+  osFile *file = os_open("memes/generic (3).png", "r");
+  printf("%d\t%d\t%s\n", file->filetype, file->inode, file->name);
   os_close(file);
   return 0;
 }
