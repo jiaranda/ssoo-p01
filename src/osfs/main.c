b5cb09ba3d19809b6e4e7f71f3858c9524f3ade2
narierao <nariera@uc.cl>
2020-10-15 23:01:01 -0300

    ✨ os_exist

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
   os_mount("data/simdiskfilled.bin");
   // os_bitmap(0, true);
-  int res = os_exists("/");
+  int res = os_exists("/memes/asdasdsadagrnd (0).png");
   printf("Resultado os_exists: %d\n", res);
   return 0;
 }
--- src/osfs/os_exists/os_exists.c
+++ src/osfs/os_exists/os_exists.c
@@ -5,66 +5,57 @@
 extern char *disk_path;
 extern uint32_t BLOCK_NUM_MASK;
 
-uint search(char *name, uint block_pointer, FILE *fp)
-{
-  unsigned char entry[32];
-  uint entry_count = 0;
-  // printf("%u\n", block_pointer);
-  fseek(fp, block_pointer * 2048, SEEK_SET);
-  while (entry_count < 64)
-  {
-    fread(entry, 32, 1, fp);
-    if (entry[0] >> 6 == 0)
-    {
-      printf("invalido\n");
-    }
-    else
-    {
-      unsigned char entry_name[29];
-      get_array_slice(entry, entry_name, 3, 31);
-      int diff = strcmp(name, (char *)entry_name);
-      printf("buscando en %s\n", entry_name);
-      if (!diff) // son iguales
-      {
-        // printf("lo encontre\n");
-        unsigned char sliced_entry[3];
-        get_array_slice(entry, sliced_entry, 0, 2);
-        // sliced_entry[0] = sliced_entry[0] >> 2;
-        // 0011111111 11111111 111111
-        return (sliced_entry[0] << 16 | sliced_entry[1] << 8 | sliced_entry[2]) & BLOCK_NUM_MASK;
-      }
-    }
-    entry_count++;
-  }
-  return 0;
-}
 
 int os_exists(char *path)
 {
-  char new_path[29];
-  strcpy(new_path, path);
+ 
+  // load disk pointer
   FILE *fp = fopen(disk_path, "rb");
   if (!fp)
   {
-    // printf("No se pudo leer el archivo\n");
+    printf("No se pudo leer el archivo\n");
     return 0;
   }
-  char *arg;
-  arg = strtok(new_path, "/");
-  uint block_pointer = 0;
-  while (arg != NULL)
+
+  // parse path
+  char tmp_path[29];
+  strcpy(tmp_path, path);
+  char *next_dir;
+  next_dir = strtok(tmp_path, "/");
+  if (!next_dir){
+    next_dir = strtok(NULL, "/");
+  }
+
+  // osFile attributes
+  unsigned char entry[32];
+  int entry_type;
+  uint32_t entry_pointer;
+  char entry_name[29];
+
+  // look for file
+  for (int i = 0; i < 64; i++)
   {
-    block_pointer = search(arg, block_pointer, fp);
-    // revisar bitmap
-    check_block_in_bitmap(block_pointer);
-    if (block_pointer == 0)
+    fread(entry, 32, 1, fp);
+    entry_type = entry[0] >> 6;
+    if (entry_type)
     {
-      fclose(fp);
-      return 0;
+      get_array_slice(entry, entry_name, 3, 31);
     }
-    arg = strtok(NULL, "/");
+
+    if (entry_type == 2 && !strcmp(entry_name, next_dir))
+    {
+      next_dir = strtok(NULL, "/");
+      i = 0;
+      entry_pointer = (entry[0] << 16 | entry[1] << 8 | entry[2]) & BLOCK_NUM_MASK;
+      fseek(fp, 2048 * entry_pointer, SEEK_SET);
+    }
+
+    if (entry_type == 1 && !strcmp(entry_name, next_dir))
+    {
+      return 1;
+    }
   }
 
   fclose(fp);
-  return 1;
+  return 0;
 }
