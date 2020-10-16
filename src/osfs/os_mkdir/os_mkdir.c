7b439dedd8baa8e390050a950f9e3f6f1d92ef14
Claudio Scheihing <cnscheihing@uc.cl>
2020-10-16 22:07:07 +0000

    🚧  os_mkdir files structure

---
+++ src/osfs/os_mkdir/os_mkdir.c
@@ -0,0 +1,7 @@
+#include "os_mkdir.h"
+
+int os_mkdir(char* path)
+{
+    printf("Ola hice os_mkdir\n");
+    return 1;
+}
---
+++ src/osfs/os_mkdir/os_mkdir.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#ifndef UTILS_H
+#define UTILS_H
+#include "./../utils/utils.h"
+#endif
+
+int os_mkdir(char* path);
