7f1f60a9803f25d81b871dc55945aae8041c0749
vaguila95 <vlaguila@uc.cl>
2020-10-17 13:48:41 -0300

    ✅  function trials organization

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -4,9 +4,18 @@ int main(int argc, char *argv[])
 {
   os_mount("data/simdiskfilled.bin");
   // os_bitmap(0, true);
-  osFile *file = os_open("/IMPORTANTS.txt", 'w');
-  printf("%d\t%s\n", file->filetype, file->name);
+
+  // os_open trials
+  osFile *file = os_open("/memes/generic (200).png", 'w');
+  if (file->filetype)
+  {
+    printf("Opened file\t%s\n", file->name);
+    printf("File type\t%d\n", file->filetype);
+    printf("Inode\t\t%d\n", file->inode);
+  }
   os_close(file);
+
+  // os_exists trials
   // int res = os_exists("/memes/generic (3).png");
   // int res = os_exists("/IMPORTANT.txt");
   // printf("Resultado os_exists: %d\n", res);
