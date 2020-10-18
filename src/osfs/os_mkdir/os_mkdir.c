8f4f6937cd5f776e9ea958f7d3b28e5916e4a94f
narierao <nariera@uc.cl>
2020-10-18 18:18:31 -0300

    🔥 remove comments & prints located in mkdir

--- src/osfs/os_mkdir/os_mkdir.c
+++ src/osfs/os_mkdir/os_mkdir.c
@@ -63,14 +63,6 @@ int os_mkdir(char *path)
     }
     if (i == 63)
     {
-      printf("ENTREEE\n");
-      /* 
-      Si termino de recorrer un directorio y no coincide nombre actual
-      - Si nombre actual es el ultimo (···/actual)
-      --- Si tiene un punto (···/actual.algo), error de archivo
-      --- Si no --> yaaaay hago mkdir
-      - Si quedan mas nombres (···/actual/foo/bar) path
-      */
       strcpy(file_name, next_dir);
       fseek(fp, 2048 * entry_pointer, SEEK_SET);
       for (int k = 0; k < 64; k++)
@@ -83,20 +75,14 @@ int os_mkdir(char *path)
           break;
         }
       }
-      // printf("fp before: %d\n", fp);
       fclose(fp);
-      // printf("fp after: %d\n", fp);
-      // printf("Estoy acaaaaa\n");
-
       char *next_final_dir = strtok(NULL, "/");
       if (!next_final_dir)
       {
         // --- Si tiene un punto (···/actual.algo), error de archivo
         if (strchr(next_dir, '.'))
         {
-          // printf("%s", strchr(entry_name, '.'));
           fprintf(stderr, "ERROR: os_mkdir. cannot create directory with file's name.\n");
-          // fclose(fp);
           return 0;
         }
         // --- Si no --> yaaaay hago mkdir
@@ -109,25 +95,20 @@ int os_mkdir(char *path)
             return 0;
           }
           // get first empty block from bitmap and use it
-          // fclose(fp);
           uint32_t empty_block = get_empty_block_pointer(1);
-
           // handle a completely filled disk
           if (!empty_block)
           {
             fprintf(stderr, "ERROR: os_mkdir. Disk is completely full.\n");
-            // fclose(fp);
             return 0;
           }
-
           // write new entry
           unsigned char new_entry_head[3];
           uint32_t tmp_head;
-          tmp_head = OS_DIRECTORY << 22 | empty_block;    // 4194368 -> 01000000 000000 01000000
-          new_entry_head[0] = tmp_head >> 16;        // esto funciona, es 64
-          new_entry_head[1] = (tmp_head >> 8) & 255; // WIP
+          tmp_head = OS_DIRECTORY << 22 | empty_block;  
+          new_entry_head[0] = tmp_head >> 16;
+          new_entry_head[1] = (tmp_head >> 8) & 255;
           new_entry_head[2] = tmp_head & 255;
-          printf("WA ESCRIBIR ENTRY\n");
           FILE *fpw = fopen(disk_path, "rb+");
           if (!fpw)
           {
@@ -136,12 +117,10 @@ int os_mkdir(char *path)
           }
           // find empty entry address
           uint32_t dir_block_number = dir_exists(path);
-          // printf("dir block number: %d\n", dir_block_number);
           int entry_number = get_empty_entry(dir_block_number);
           if (entry_number == -1)
           {
             printf("ERRROR: os_mkdir. Couldn't find empty entry\n");
-            // fclose(fp);
             return 0;
           }
           // write new entry
@@ -149,21 +128,16 @@ int os_mkdir(char *path)
           fwrite(new_entry_head, 3, 1, fpw);
           fwrite(file_name, 29, 1, fpw);
           fclose(fpw);
-          // fclose(fp);
           return 1;
-
         }
       }
       else
       {
         // - Si quedan mas nombres (···/ACTUAL/foo/bar) path malo
         fprintf(stderr, "ERROR: el path está incorrecto.\n");
-        // fclose(fp);
         return 0;
       }
     }
   }
-  // fclose(fp);
-  // printf("Ah taba el mkdir\n");
   return 0;
 }
