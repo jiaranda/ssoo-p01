887dc17faf12aa76f2bcb80dcd6c08d1a5ee588b
Javier <jiaranda@uc.cl>
2020-10-17 23:08:29 -0300

    ♻️ clean main

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -3,30 +3,16 @@
 int main(int argc, char *argv[])
 {
   os_mount("data/simdiskfilled.bin");
-  // os_bitmap(0, true);
 
-  // os_open trials
-  // osFile *file = os_open("/memes/generic (201).png", 'w');
-  // if (file->filetype)
-  // {
-  //   printf("Opened file\t%s\n", file->name);
-  //   printf("File type\t%d\n", file->filetype);
-  //   printf("Inode\t\t%d\n", file->inode);
-  // }
-  // os_close(file);
-
-  for (uint32_t i = 0; i < 66; i++)
+  osFile *file = os_open("/memes/generic (123).png", 'w');
+  if (file->filetype)
   {
-    char buffer[29];
-    sprintf(buffer, "memes/meme %d.jpg", i);
-    osFile *file = os_open(buffer, 'w');
-    os_close(file);
+    printf("Opened file\t%s\n", file->name);
+    printf("File type\t%d\n", file->filetype);
+    printf("Inode\t\t%d\n", file->inode);
   }
-  os_ls("/memes");
+  os_close(file);
 
-  // os_exists trials
-  // int res = os_exists("/memes/generic (3).png");
-  // int res = os_exists("/memes/generic (200).png");
-  // printf("Resultado os_exists: %d\n", res);
+  os_ls("/memes");
   return 0;
 }
